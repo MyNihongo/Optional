2a842d2444b34c13d110580ca210e9468306883a
My Nihongo <65362546+my-nihongo@users.noreply.github.com>
2021-03-09 21:59:23 +0200

    Add files via upload

---
+++ src/Extensions/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿/*
+* Copyright © 2021 MyNihongo
+*/
+
+using System.Threading.Tasks;
+
+namespace MyNihongo.Option.Extensions
+{
+	public static class ObjectExtensions
+	{
+		public static Optional<T> AsOptional<T>(this T @this) =>
+			Optional<T>.Of(@this);
+
+		public static async ValueTask<Optional<T>> AsOptionalAsync<T>(this ValueTask<T> @this) =>
+			await @this.ConfigureAwait(false);
+
+		public static async Task<Optional<T>> AsOptionalAsync<T>(this Task<T> @this) =>
+			await @this.ConfigureAwait(false);
+	}
+}
