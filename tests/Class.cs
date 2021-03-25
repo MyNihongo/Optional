7576638529725f1091d7cd6c373814f4f7cd9bfa
My Nihongo <65362546+my-nihongo@users.noreply.github.com>
2021-03-25 16:27:29 +0200

    Create Class.cs

---
+++ tests/Class.cs
@@ -0,0 +1,13 @@
+/*
+* Copyright © 2021 MyNihongo
+*/
+
+namespace MyNihongo.Option.Tests
+{
+	public sealed class Class
+	{
+		public int Id { get; init; }
+
+		public string Name { get; init; }
+	}
+}
