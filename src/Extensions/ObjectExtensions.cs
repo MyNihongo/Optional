26aed77dda19beceb2b9ee817edffda8dafe052f
My Nihongo <65362546+my-nihongo@users.noreply.github.com>
2021-03-31 16:26:30 +0300

    add AsNonNullOptional

--- src/Extensions/ObjectExtensions.cs
+++ src/Extensions/ObjectExtensions.cs
@@ -19,5 +19,37 @@ namespace MyNihongo.Option.Extensions
 
 		public static async Task<Optional<T>> AsOptionalAsync<T>(this Task<T> @this) =>
 			await @this.ConfigureAwait(false);
+		
+		public static Optional<T> AsNonNullOptional<T>(this T @this) =>
+			@this != null
+				? Optional<T>.Of(@this)
+				: Optional<T>.None();
+
+		public static ValueTask<Optional<T>> AsNonNullOptionalAsync<T>(this T @this)
+		{
+			var result = @this != null
+				? Optional<T>.Of(@this)
+				: Optional<T>.None();
+
+			return new ValueTask<Optional<T>>(result);
+		}
+
+		public static async ValueTask<Optional<T>> AsNonOptionalAsync<T>(this ValueTask<T> @this)
+		{
+			var result = await @this.ConfigureAwait(false);
+			
+			return result != null
+				? Optional<T>.Of(result)
+				: Optional<T>.None();
+		}
+
+		public static async ValueTask<Optional<T>> AsNonOptionalAsync<T>(this Task<T> @this)
+		{
+			var result = await @this.ConfigureAwait(false);
+
+			return result != null
+				? Optional<T>.Of(result)
+				: Optional<T>.None();
+		}
 	}
 }
